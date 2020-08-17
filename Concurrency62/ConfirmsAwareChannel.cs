a7f03e274f9a75e79ee6a4f1725b3f52fd52a28b
danielmarbach <daniel.marbach@openplace.net>
2020-08-17 15:49:25 +0200

    Add Concurrency sample with latest client

---
+++ Concurrency62/ConfirmsAwareChannel.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Concurrency
+{
+    sealed class ConfirmsAwareChannel : IDisposable
+    {
+        public ConfirmsAwareChannel(IConnection connection)
+        {
+            channel = connection.CreateModel();
+            channel.BasicAcks += Channel_BasicAcks;
+            channel.BasicNacks += Channel_BasicNacks;
+            channel.BasicReturn += Channel_BasicReturn;
+            channel.ModelShutdown += Channel_ModelShutdown;
+
+            channel.ConfirmSelect();
+
+            messages = new ConcurrentDictionary<ulong, TaskCompletionSource<bool>>();
+        }
+
+        public IBasicProperties CreateBasicProperties()
+        {
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = new Dictionary<string, object>();
+            return properties;
+        }
+
+        public bool IsOpen => channel.IsOpen;
+
+        public bool IsClosed => channel.IsClosed;
+
+        public Task SendMessage(string address, string message, IBasicProperties properties)
+        {
+            try
+            {
+                var task = GetConfirmationTask();
+                properties.SetConfirmationId(channel.NextPublishSeqNo);
+
+                channel.BasicPublish(address, string.Empty, true, properties, Encoding.UTF8.GetBytes(message));
+                return task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
+
+        private Task GetConfirmationTask()
+        {
+            var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var added = messages.TryAdd(channel.NextPublishSeqNo, tcs);
+
+            if (!added)
+            {
+                throw new Exception($"Cannot publish a message with sequence number '{channel.NextPublishSeqNo}' on this channel. A message was already published on this channel with the same confirmation number.");
+            }
+
+            return tcs.Task;
+        }
+
+        void Channel_BasicAcks(object sender, BasicAckEventArgs e)
+        {
+            if (!e.Multiple)
+            {
+                SetResult(e.DeliveryTag);
+            }
+            else
+            {
+                foreach (var message in messages)
+                {
+                    if (message.Key <= e.DeliveryTag)
+                    {
+                        SetResult(message.Key);
+                    }
+                }
+            }
+        }
+
+        void Channel_BasicNacks(object sender, BasicNackEventArgs e)
+        {
+            if (!e.Multiple)
+            {
+                SetException(e.DeliveryTag, "Message rejected by broker.");
+            }
+            else
+            {
+                foreach (var message in messages)
+                {
+                    if (message.Key <= e.DeliveryTag)
+                    {
+                        SetException(message.Key, "Message rejected by broker.");
+                    }
+                }
+            }
+        }
+
+        void Channel_BasicReturn(object sender, BasicReturnEventArgs e)
+        {
+            var message = $"Message could not be routed to {e.Exchange + e.RoutingKey}: {e.ReplyCode} {e.ReplyText}";
+
+            if (e.BasicProperties.TryGetConfirmationId(out var deliveryTag))
+            {
+                SetException(deliveryTag, message);
+            }
+            else
+            {
+                // omit demo
+            }
+        }
+
+        void Channel_ModelShutdown(object sender, ShutdownEventArgs e)
+        {
+            do
+            {
+                foreach (var message in messages)
+                {
+                    SetException(message.Key, $"Channel has been closed: {e}");
+                }
+            } while (!messages.IsEmpty);
+        }
+
+        void SetResult(ulong key)
+        {
+            if (messages.TryRemove(key, out var tcs))
+            {
+                tcs.SetResult(true);
+            }
+        }
+
+        void SetException(ulong key, string exceptionMessage)
+        {
+            if (messages.TryRemove(key, out var tcs))
+            {
+                tcs.SetException(new Exception(exceptionMessage));
+            }
+        }
+
+        public void Dispose()
+        {
+            channel?.Dispose();
+        }
+
+        IModel channel;
+
+        readonly ConcurrentDictionary<ulong, TaskCompletionSource<bool>> messages;
+
+    }
+}
---
+++ Concurrency62/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Concurrency
+{
+    class Program
+    {
+        private const string InputQueue = "basic-bits";
+        private const string ConsumerTag = "basic-bits";
+
+        static async Task Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UseBackgroundThreadsForIO = true,
+                DispatchConsumersAsync = true,
+                ProcessingConcurrency = 4,
+                // ConsumerDispatchConcurrency = 4,
+            };
+
+            #region TopologyAndSending
+
+            CreateTopologyIfNecessary(InputQueue, connectionFactory);
+
+            var cts = new CancellationTokenSource();
+            var sendConnection = connectionFactory.CreateConnection($"{InputQueue} sender");
+            var senderChannel = new ConfirmsAwareChannel(sendConnection);
+            var sendMessagesTask = Task.Run(() => SendMessages(cts, senderChannel, InputQueue), CancellationToken.None);
+
+            var receiveConnection = connectionFactory.CreateConnection($"{InputQueue} pump");
+            var receiveChannel = receiveConnection.CreateModel();
+
+            #endregion
+
+            receiveChannel.BasicQos(prefetchSize: 0, prefetchCount: 10, global: false);
+
+            var consumer = new AsyncEventingBasicConsumer(receiveChannel);
+
+            consumer.Registered += Consumer_Registered;
+            receiveConnection.ConnectionShutdown += Connection_ConnectionShutdown;
+
+            var exclusiveScheduler = new ConcurrentExclusiveSchedulerPair().ExclusiveScheduler;
+
+            consumer.Received += (sender,
+                deliverEventArgs) => Consumer_Received(deliverEventArgs,
+                receiveChannel,
+                exclusiveScheduler,
+                cts.Token);
+
+            receiveChannel.BasicConsume(InputQueue, false, ConsumerTag, consumer);
+
+            #region Stop
+
+            await Console.Error.WriteLineAsync("Press any key to stop");
+            Console.ReadLine();
+
+            cts.Cancel();
+
+            await sendMessagesTask;
+
+            receiveChannel.Close();
+            receiveConnection.Close();
+            senderChannel.Dispose();
+            sendConnection.Close();
+
+            #endregion
+        }
+
+        private static async Task SendMessages(CancellationTokenSource cts, ConfirmsAwareChannel senderChannel,
+            string inputQueue)
+        {
+            var messageNumber = 0;
+            while (!cts.IsCancellationRequested)
+            {
+                var properties = senderChannel.CreateBasicProperties();
+                await senderChannel.SendMessage(inputQueue, messageNumber++.ToString(), properties);
+                await Task.Delay(250);
+            }
+        }
+
+        private static async Task Consumer_Received(
+            BasicDeliverEventArgs e,
+            IModel channel,
+            TaskScheduler exclusiveScheduler,
+            CancellationToken cancellationToken)
+        {
+            await Console.Out.WriteLineAsync($"v: {Encoding.UTF8.GetString(e.Body.Span)} / q: {channel.MessageCount(InputQueue)}");
+
+            await Task.Delay(1000, CancellationToken.None);
+
+            await channel.BasicAckSingle(e.DeliveryTag, exclusiveScheduler);
+        }
+
+        #region NotRelevant
+
+        private static Task Consumer_Registered(object? sender, ConsumerEventArgs e)
+        {
+            Console.Error.WriteLine($"Consumer(s) '{string.Join(" ", e.ConsumerTags)}' registered");
+            return Task.CompletedTask;
+        }
+
+        private static void Connection_ConnectionShutdown(object? sender, ShutdownEventArgs e)
+        {
+            Console.Error.WriteLine("Shutdown");
+        }
+
+        private static void CreateTopologyIfNecessary(string queue, IConnectionFactory connectionFactory)
+        {
+            var adminConnection = connectionFactory.CreateConnection("Admin connection");
+            var channel = adminConnection.CreateModel();
+            channel.QueueDeclare(queue, durable: true, false, false, null);
+            CreateExchange(queue);
+            channel.QueueBind(queue, queue, string.Empty);
+            channel.QueuePurge(queue);
+            channel.Close();
+            adminConnection.Close();
+
+            void CreateExchange(string exchangeName)
+            {
+                try
+                {
+                    channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, durable: true);
+                }
+                // ReSharper disable EmptyGeneralCatchClause
+                catch (Exception)
+                    // ReSharper restore EmptyGeneralCatchClause
+                {
+                    // TODO: Any better way to make this idempotent?
+                }
+            }
+        }
+
+        #endregion
+    }
+}
